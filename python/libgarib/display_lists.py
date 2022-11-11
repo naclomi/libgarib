d7215019c8a34ae9d0526ff6f6ee9093e18c6afd
nacl <uhohnaomi@gmail.com>
2022-11-10 17:28:40 -0800

    begin work on display list export

--- python/libgarib/display_lists.py
+++ python/libgarib/display_lists.py
@@ -1,6 +1,71 @@
 import json
 import struct
-from .gbi import F3DEX
+from .gbi import F3DEX, Vertex as GbiVertex
+
+def f3dexToGltf(display_list, bank):
+    raw_dl = b"".join(struct.pack(">II", cmd.w1, cmd.w0) for cmd in mesh.display_list)
+
+    vertex_buffer = [GbiVertex() for _ in range(32)]
+
+    for cmd, args in F3DEX.parseList(raw_dl):
+        if cmd is F3DEX.byName["G_VTX"]:
+            n_dma_bytes = GbiVertex.LENGTH * args["n"]
+            write_idx = args["v0"]
+            for read_idx in range(args["n"]):
+                vertex_bytes = bank[args["address"] + read_idx*GbiVertex.LENGTH:
+                                         args["address"] + (read_idx+1)*GbiVertex.LENGTH]
+                vertex_buffer[write_idx].unpack(vertex_bytes)
+                write_idx += 1
+        elif cmd is F3DEX.byName["G_MODIFYVTX"]:
+            v = vertex_buffer[args["vtx"]]
+            if args["where"] == "G_MWO_POINT_RGBA":
+                v.setNormRGBA(args["val"])
+            elif args["where"] == "G_MWO_POINT_ST":
+                v.setUV(args["val"])
+            elif args["where"] == "G_MWO_POINT_XYSCREEN":
+                v.setXY(args["val"])
+            elif args["where"] == "G_MWO_POINT_ZSCREEN":
+                v.setZ(args["val"])
+            else:
+                raise Exception("Bad G_MODIFYVTX 'where'")
+        elif cmd is F3DEX.byName["G_TRI1"]:
+            # v0, v1, v2
+            # buffer = (
+            #     self.vertices[v0].asGLBytes(self.lightingEnabled) +
+            #     self.vertices[v1].asGLBytes(self.lightingEnabled) +
+            #     self.vertices[v2].asGLBytes(self.lightingEnabled))
+            # if self.renderBackfaces:
+            #     self.ctx.disable(moderngl.CULL_FACE)
+            # self.vbo.write(buffer)
+            # self.configureTextures()
+            # self.vao.render(vertices=3)
+            # return True
+
+            # TODO
+            ...
+        elif cmd is F3DEX.byName["G_TRI2"]:
+            # v00, v01, v02, v10, v11, v12
+            # buffer = (
+            #     self.vertices[v00].asGLBytes(self.lightingEnabled) +
+            #     self.vertices[v01].asGLBytes(self.lightingEnabled) +
+            #     self.vertices[v02].asGLBytes(self.lightingEnabled) +
+            #     self.vertices[v10].asGLBytes(self.lightingEnabled) +
+            #     self.vertices[v11].asGLBytes(self.lightingEnabled) +
+            #     self.vertices[v12].asGLBytes(self.lightingEnabled))
+            # if self.renderBackfaces:
+            #     self.ctx.disable(moderngl.CULL_FACE)
+            # self.vbo.write(buffer)
+            # self.configureTextures()
+            # self.vao.render(vertices=6)
+
+            # TODO
+            ...
+        elif cmd is F3DEX.byName["G_ENDDL"]:
+            break
+        else:
+            raise Exception("TODO: Not yet implemented: Export F3DEX command {:}".format(cmd))
+        offset += 8
+
 
 
 def dump_f3dex_dl(mesh, bank):
