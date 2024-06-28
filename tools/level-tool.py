1c79282c202120526fc059cd9a091b06b339dfb7
Naomi Alterman <naomila@uw.edu>
2024-06-28 10:42:42 -0700

    Style work

--- tools/level-tool.py
+++ tools/level-tool.py
@@ -11,11 +11,18 @@ import yaml
 import _prefer_local_implementation
 from libgarib.parsers.glover_level import GloverLevel
 
+
 def assemble(args):
     pass
 
+
 def disassemble(args):
     pass
+
+
+def validate(args):
+    pass
+
 
 def schema(args):
     with open(args.ksy_file, "r") as f:
@@ -23,26 +30,41 @@ def schema(args):
         for type_name, type_def in ksy["types"].items():
             print(type_name)
 
-if __name__=="__main__":
-    parser = argparse.ArgumentParser(description="Tool to work with landscape files from Glover (N64)")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Tool to work with landscape files from Glover (N64)")
     subparsers = parser.add_subparsers(dest="command", required=True)
 
-    asm_parser = subparsers.add_parser('assemble', help='Build binary lev files from XML level data')
-    asm_parser.add_argument("level_xml_file", type=str, nargs="+",
-                        help="Level file in XML format to assemble into binary lev data")
-    asm_parser.add_argument("--output-dir", type=str, default=os.getcwd(),
-                        help="Directory to output binary level data")
+    asm_parser = subparsers.add_parser(
+        'assemble', help='Build binary lev files from XML level data')
+    asm_parser.add_argument(
+        "level_xml_file", type=str, nargs="+",
+        help="Level file in XML format to assemble into binary lev data")
+    asm_parser.add_argument(
+        "--output-dir", type=str, default=os.getcwd(),
+        help="Directory to output binary level data")
 
-    disasm_parser = subparsers.add_parser('disassemble', help='Extract XML level data from binary Glover level files')
-    disasm_parser.add_argument("level_binary_file", type=str, nargs="+",
-                        help="Binary level file")
-    disasm_parser.add_argument("--output-dir", type=str, default=os.getcwd(),
-                        help="Directory to output XML level data")
+    disasm_parser = subparsers.add_parser(
+        'disassemble', help='Extract XML level data from binary Glover level files')
+    disasm_parser.add_argument(
+        "level_binary_file", type=str, nargs="+",
+        help="Binary level file")
+    disasm_parser.add_argument(
+        "--output-dir", type=str, default=os.getcwd(),
+        help="Directory to output XML level data")
 
-    schema_parser = subparsers.add_parser('xml-schema', help='Extract XML schema for level data from Kaitai level format')
-    schema_parser.add_argument("ksy_file", type=str,
-                        help="Kaitai structure definition of Glover level format")
+    validate_parser = subparsers.add_parser(
+        'validate', help='Validate integrity and design rules of a level')
+    validate_parser.add_argument(
+        "level_or_xml_file", type=str,
+        help="Glover level, either in binary format or XML")
 
+    schema_parser = subparsers.add_parser(
+        'xml-schema', help='Extract XML schema for level data from Kaitai level format')
+    schema_parser.add_argument(
+        "ksy_file", type=str,
+        help="Kaitai structure definition of Glover level format")
 
     args = parser.parse_args()
     if args.command == "assemble":
@@ -51,3 +73,5 @@ if __name__=="__main__":
         disassemble(args)
     elif args.command == "xml-schema":
         schema(args)
+    elif args.command == "validate":
+        validate(args)
